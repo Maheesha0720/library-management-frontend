f570c966a2d74a1145ba479807223dc4050268a6
Maheesha0720 <maheeshamaduwanthi2002@gmail.com>
2025-01-05 13:57:50 +0530

    sidebar changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../assets/css/sidebar.css'
+import dashboard from '../assets/img/dashboard.png'
+import books from '../assets/img/books.png'
+import publishing from '../assets/img/publishing.png'
+import writer from '../assets/img/writer.png'
+import profile from '../assets/img/profile.png'
+import borrow from '../assets/img/borrow.png'
+
 
 function Sidebar() {
   return (
     <aside className="sidebar">
       <ul>
-        <li><Link to="/dashboard"> Dashboard</Link></li>
-        <li><Link to="/books"> Books</Link></li>
-        <li><Link to="/authors"> Authors</Link></li>
-        <li><Link to="/publishers"> Publishers</Link></li>
-        <li><Link to="/borrows"> Borrows</Link></li>
-        <li><Link to="/users"> Users</Link></li>
+        <li><Link to="/dashboard"><img src={dashboard} alt="" /><br /> Dashboard</Link></li>
+        <li><Link to="/books"><img src={books} alt="" /> <br />Books</Link></li>
+        <li><Link to="/authors"> <img src={writer} alt="" /> <br />Authors</Link></li>
+        <li><Link to="/publishers"><img src={publishing} alt="" /> <br /> Publishers</Link></li>
+        <li><Link to="/borrows"><img src={borrow} alt="" /> <br /> Borrows</Link></li>
+        <li><Link to="/users"><img src={profile} alt="" /> <br /> Users</Link></li>
       </ul>
     </aside>
   );
